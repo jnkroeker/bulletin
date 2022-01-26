a640cf88d770f42859c0e83ca76f6b23cb2bdbcc
john <jnkroeker@gmail.com>
2022-01-25 19:32:08 -0500

    sidebar blocks menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import { createTheme, makeStyles } from '@material-ui/core';
+import {useState} from 'react';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import './App.css';
+import clsx from 'clsx';
+import { Header } from './components/Header';
+import { ContentContainer } from './components/ContentContainer';
+import { Sidebar } from './components/Sidebar';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth 
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: 0
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? "dark" : "light"
+    }
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline/>
+        <Header 
+          handleDrawerToggle={handleDrawerToggle}
+          toggleDarkMode={toggleDarkMode}
+          darkMode={darkMode}
+        />
+        <Sidebar handleDrawerClose={handleDrawerClose} open={open}/>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open
+          })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className={classes.drawerHeader}/>
+          <ContentContainer/>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/DisplayContainer.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent, useState } from "react";
+
+export const DisplayContainer: FunctionComponent = ({}) => {
+
+    return (
+        <div>
+            <div>
+                This is where to game goes
+            </div>
+        </div>
+    )
+}
